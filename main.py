5651d70872bd968279c99da3d4880ce72b631338
U-1055 <filatov_truba@mail.ru>
2025-05-31 21:05:41 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+from tkinter import Frame, Tk, BOTH, N, S, W, E, NORMAL, DISABLED
+from customtkinter import CTkComboBox, CTkButton
+import json
+import datetime
+from threading import Thread
+
+class Window:
+    def __init__(self):
+        PATH = ''
+        self.place_widgets()
+
+    def place_widgets(self):
+        wdg_frame = Frame(master)
+        wdg_frame.pack(anchor=W)
+        wdg_deeds_today = ComboBox(wdg_frame)
+        wdg_deeds_today.grid(row=0, column=0, columnspan=2, sticky=W+E)
+
+        wdg_btn_start = CTkButton(wdg_frame, text='Начать выполнение')
+        wdg_btn_start.grid(row=1, column=0)
+
+        wdg_btn_stop = CTkButton(wdg_frame, text='Приостановить выполнение')
+        wdg_btn_stop.grid(row=1, column=1)
+
+    def start_deed(self):
+        pass
+
+    def stop_deed(self):
+        pass
+
+    def start_break(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
+class ComboBox(CTkComboBox):
+    def __init__(self, master):
+        super().__init__(master=master, state='readonly')
+        self.configure(values=[])
+        self.bind('<Enter>', self.to_adding_mode)
+        self.deeds_list = DeedsList(master)
+        self.deeds_list.pack
+    def to_adding_mode(self, event):
+
+        self.binding = self.bind('<ButtonPress-1>', self.add_value)
+        self.bind('<Leave>', self.to_normal_mode)
+        self.configure(state=NORMAL)
+        self.focus_get()
+
+    def to_normal_mode(self, event):
+        self.configure(state='readonly')
+        self.unbind(self.binding) # Невозможен случай, при котором binding не будет объявлен к этому моменту
+        self.bind('<Enter>', self.to_adding_mode)
+    def add_value(self, event):
+        value = self.get()
+        values = self.cget('values')
+        if not (value == '' or value in values):
+            values.append(value)
+            self.configure(values=values)
+
+class DeedsList(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+    def add_deed(self):
+        pass
+
+if __name__ == '__main__':
+
+    root = Tk()
+    root.title('TimeTracker')
+    root.geometry(f'{root.winfo_screenwidth() // 100 * 60}x{root.winfo_screenheight() // 100 * 60}'
+                  f'+{root.winfo_screenwidth() // 100 * 20}+{root.winfo_screenheight() // 100 * 20}')
+
+    master = Frame(root)
+    master.pack(fill=BOTH, expand=True)
+    Window()
+
+    root.mainloop()
